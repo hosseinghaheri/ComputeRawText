af8915c4926dd9d06dfbed63ec3781af0d3dd7bd
Hossein Ghaheri <hgdownloadcenter@gmail.com>
2023-07-19 09:25:25 +0330

    Add project files.

---
+++ ComputeRawText/Extensions/StringExtensions.cs
@@ -0,0 +1,65 @@
+﻿using NCalc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeRawText.Extensions
+{
+    public static class StringExtensions
+    {
+
+        /// <summary>
+        /// Simple Compute using Datatable Compute
+        /// </summary>
+        /// <param name="expression">2+2</param>
+        /// <returns></returns>
+        public static string SimpleCompute(this string expression)
+        {
+            try
+            {
+                return Convert.ToDouble(new DataTable().Compute(expression, String.Empty)).ToString();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+
+        /// <summary>
+        /// Ultra Compute using Ncalc
+        /// </summary>
+        /// <param name="expression">Raw math text</param>
+        /// <param name="parameters">Object of parameters</param>
+        /// <returns></returns>
+        public static string Compute(this string expression, object parameters = null)
+        {
+            try
+            {
+                Expression expr = new Expression(expression);
+                if (parameters != null)
+                {
+                    Type objectType = parameters.GetType();
+                    PropertyInfo[] properties = objectType.GetProperties();
+
+                    foreach (PropertyInfo property in properties)
+                    {
+                        string propertyName = property.Name;
+                        object propertyValue = property.GetValue(parameters, null);
+                        expr.Parameters[propertyName] = propertyValue;
+                    }
+                }
+                object result = expr.Evaluate();
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ ComputeRawText/Program.cs
@@ -0,0 +1,26 @@
+﻿using ComputeRawText.Extensions;
+using System;
+
+namespace ComputeRawText
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var eq = "2+2";
+            var simpleCompute = eq.SimpleCompute();
+            Console.WriteLine($"Simple Compute:\r\n\t{eq}= {simpleCompute}");
+
+            eq = "x < 0 ? Abs(x) : x * 2";
+            string result1 = eq.Compute(new { x = -4 });
+            string result2 = eq.Compute(new { x = 7 });
+
+            Console.WriteLine($"Ultra Compute:\r\n\t{eq}");
+            Console.WriteLine($"\t\tx=-4 -> {result1}");
+            Console.WriteLine($"\t\tx=7 -> {result2}");
+
+            Console.ReadKey();
+        }
+    }
+}
